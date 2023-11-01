351020ce992b91a7ff6c970280d5d7cbabb142d5
Nghia Nguyen <nnnghia98@gmail.com>
2023-11-01 14:17:51 +0700

    added config

--- src/config.js
+++ src/config.js
@@ -20,17 +20,6 @@ const data = {
       pointHoverBackgroundColor: "#fff",
       pointHoverBorderColor: "rgb(255, 99, 132)",
     },
-    {
-      label: "My Second Dataset",
-      data: [28, 48, 40, 19, 96, 27, 100],
-      fill: true,
-      backgroundColor: "rgba(54, 162, 235, 0.2)",
-      borderColor: "rgb(54, 162, 235)",
-      pointBackgroundColor: "rgb(54, 162, 235)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgb(54, 162, 235)",
-    },
   ],
 };
 
