159d2b8b592669b5d3a40edac5ef81bf9d4732a3
Mai Tien <mtien@strongtie.com>
2023-11-10 09:00:13 +0700

    create function to config dataset

--- src/config.js
+++ src/config.js
@@ -1,31 +1,23 @@
-const data = {
-  labels: [
-    "Eating",
-    "Drinking",
-    "Sleeping",
-    "Designing",
-    "Coding",
-    "Cycling",
-    "Running",
-  ],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [65, 59, 90, 81, 56, 55, 40],
-      fill: true,
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      borderColor: "rgb(255, 99, 132)",
-      pointBackgroundColor: "rgb(255, 99, 132)",
-      pointBorderColor: "#fff",
-      pointHoverBackgroundColor: "#fff",
-      pointHoverBorderColor: "rgb(255, 99, 132)",
-    },
-  ],
+import generateRadarDataset from "./renderDataSetChart.js";
+
+
+const userLabels = ["Drinking", "Coding", "Eating"];
+const userDataSet = [45, 50, 10];
+const userDataSetLabel = 'My Dataset';
+
+const userOptions = {
+  fill: true,
+  backgroundColor: "rgba(75, 192, 192, 0.2)",
+  borderColor: "rgb(75, 192, 192)",
+  pointBackgroundColor: "rgb(75, 192, 192)",
+  pointHoverBorderColor: "rgb(75, 192, 192)",
 };
+
+const userRadarDataset = generateRadarDataset(userLabels, userDataSet, userDataSetLabel, userOptions)
 
 const chartOptions = {
   type: "radar",
-  data: data,
+  data: userRadarDataset,
   options: {
     elements: {
       line: {
