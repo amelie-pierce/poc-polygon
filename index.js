5b5449c978ccbbd86880df37f8116fc1b30bf54e
Nghia Nguyen <nnnghia98@gmail.com>
2024-04-10 13:53:59 +0700

    build mode dev and prod

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { createOption } from "./src/option.js";
 
-// // Default init echarts
-// var chartDom = document.getElementById("chart-container");
+// Default init echarts
+var chartDom = document.getElementById("chart-container");
 
-// var myChart = echarts.init(chartDom);
+var myChart = echarts.init(chartDom);
 
-// var option = createOption();
+var option = createOption();
 
-// option && myChart.setOption(option);
+option && myChart.setOption(option);
 
-// window.addEventListener("resize", function () {
-//   myChart.resize();
-// });
+window.addEventListener("resize", function () {
+  myChart.resize();
+});
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,29 +1,43 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: ["./echarts.min.js", "./src/option.js"],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    library: {
-      name: "polygon",
-      type: "umd",
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+module.exports = (_, argv) => {
+  const entry = ["./echarts.min.js"];
+
+  switch (argv.mode) {
+    case "development":
+    default:
+      entry.push("./src/option.js");
+      break;
+    case "production":
+      entry.push("./index.js");
+      break;
+  }
+
+  return {
+    entry: entry,
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      library: {
+        name: "polygon",
+        type: "umd",
       },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      argv.mode === "production" &&
+        new HtmlWebpackPlugin({
+          template: "./index.html",
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-  ],
+  };
 };
