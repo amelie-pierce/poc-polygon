751062e633610f4f43bef02f92a7aa4a128f68ca
Nghia Nguyen <nnnghia98@gmail.com>
2024-06-07 13:55:06 +0700

    Apply multiple chart line & Responsive chart

--- src/actions/values.js
+++ src/actions/values.js
@@ -1,4 +1,6 @@
-export const parseValueArray = (size, divisor, layerOrder) => [
+import { DIVISOR } from "../shared/constants.js";
+
+export const parseValueArray = ({ size, divisor = DIVISOR, layerOrder }) => [
   size - (size / divisor) * layerOrder,
   size - (size / divisor) * layerOrder,
   size - (size / divisor) * layerOrder,
@@ -7,12 +9,12 @@ export const parseValueArray = (size, divisor, layerOrder) => [
   size - (size / divisor) * layerOrder,
 ];
 
-export const createValueLine = (values) => ({
+export const createValueLine = ({ value, name }) => ({
   type: "radar",
   radarIndex: 0,
   data: [
     {
-      value: values,
+      value: value,
       symbol: "circle",
       symbolSize: 0,
       label: {
@@ -22,6 +24,31 @@ export const createValueLine = (values) => ({
         type: "solid",
         width: 2,
       },
+      name: name,
     },
   ],
 });
+
+export const parseValueLine = (values) => {
+  const order = {
+    0: "technology",
+    1: "influence",
+    2: "process",
+    3: "people",
+    4: "system",
+  };
+
+  const arrayValues = [];
+
+  for (let i = 0; i <= 4; i++) {
+    let value = values[order[i]];
+
+    if (!value || value < 0) {
+      value = 0;
+    }
+
+    arrayValues[i] = value;
+  }
+
+  return arrayValues;
+};
